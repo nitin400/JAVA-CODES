08435cffe38b6a2d2f81ff752436fb8f4ef752e1
nitin <nitin.andhale26@gmail.com>
2023-04-25 22:48:01 +0530

    command line argument class code

---
+++ class code/COMMAND LINE ARGUMENT/Program2.java
@@ -0,0 +1,8 @@
+public class Program2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println(args.length);
+    }
+    
+}
---
+++ class code/COMMAND LINE ARGUMENT/program3.java
@@ -0,0 +1,7 @@
+public class program3 {
+
+    public static void main(String... args){
+        System.out.println("Nitin Parasram andhale");
+    }
+    
+}
