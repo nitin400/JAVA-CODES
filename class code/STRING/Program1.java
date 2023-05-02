e5f2b4c087c84ed2c19aff52acb4596e02b85672
nitin <nitin.andhale26@gmail.com>
2023-05-02 16:15:11 +0530

    string

---
+++ class code/STRING/Program1.java
@@ -0,0 +1,12 @@
+public class Program1{
+    
+    public static void main(String[] args){
+        String str1="Core2Web";
+        String str2=new String("Core2Web");
+        char str3[]={'C','2','W'};
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+
+    }
+}
