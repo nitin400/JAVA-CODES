184fd45eee8e702ba5e733017579a50ce4f338a8
nitin <nitin.andhale26@gmail.com>
2023-05-11 13:49:08 +0530

    class code 10_05_2023

---
+++ class code/OOP/10_05_lecture/ClassesDemo.java
@@ -0,0 +1,15 @@
+class cls {
+
+    static int x = 10;
+    cls(){
+        System.out.println("Nitin");
+    }
+
+    public static void main(String[] args) {
+        int y = 10;
+        cls cc=new cls();
+
+
+    }
+
+}
