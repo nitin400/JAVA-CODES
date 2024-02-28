6d8d3753ee3cb6c362905d1e9c74c57dd294c1ef
nitin400 <nitin.andhale26@gmail.com>
2024-02-28 19:02:15 +0530

    DAY 45

---
+++ CODING CHALLENGE C2W/DAY 45/gfg.java
@@ -0,0 +1,21 @@
+class Solution {
+    void rotateMatrix(int matrix[][], int n) {
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Reverse each column
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+        }
+    }
+}
