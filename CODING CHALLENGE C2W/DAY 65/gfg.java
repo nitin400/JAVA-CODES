cc1c04066bceb51e49e6febffe959cdb454d4c3f
nitin400 <nitin.andhale26@gmail.com>
2024-03-19 21:46:40 +0530

    DAY 65

---
+++ CODING CHALLENGE C2W/DAY 65/gfg.java
@@ -0,0 +1,40 @@
+class GfG {
+    static class Node {
+        int data;
+        Node next;
+        Node bottom;
+
+        Node(int d) {
+            data = d;
+            next = null;
+            bottom = null;
+        }
+    }
+
+    Node merge(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node result;
+
+        if (a.data < b.data) {
+            result = a;
+            result.bottom = merge(a.bottom, b);
+        } else {
+            result = b;
+            result.bottom = merge(a, b.bottom);
+        }
+
+        return result;
+    }
+
+    Node flatten(Node root) {
+        if (root == null || root.next == null) return root;
+
+        root.next = flatten(root.next);
+
+        root = merge(root, root.next);
+
+        return root;
+    }
+}
